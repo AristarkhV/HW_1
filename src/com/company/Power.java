2206095bcbe8527d6e9e68becbfe7969a8317a6a
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-05-30 11:02:13 +0300

    update

--- src/com/company/Power.java
+++ src/com/company/Power.java
@@ -1,10 +1,10 @@
 package com.company;
 public class Power extends Service {
-    static double getPower(int Number, int Power) {
+    static double getPower(int number, int power) {
         double result = 1;
-        for (int i = 1; i <= Math.abs(Power); i++)
-                result = result * Number;
-        if (Power < 0)
+        for (int i = 1; i <= Math.abs(power); i++)
+                result = result * number;
+        if (power < 0)
                 result = 1 / result;
         return result;
     }
